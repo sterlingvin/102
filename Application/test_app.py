ff44be4fbadda60b51b3f9d17175e4935f1bbd51
vinay <sterlingvinay01@gmail.com>
2024-08-16 11:12:21 -0400

    python application

---
+++ Application/test_app.py
@@ -0,0 +1,16 @@
+# test_app.py
+import unittest
+from app import app
+
+class FlaskAppTests(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode(), "Hello, World!")
+
+if __name__ == '__main__':
+    unittest.main()
